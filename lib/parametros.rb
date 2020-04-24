a9fa8e301fe1b2b8286593e9d61f2aa0f48a7d4c
bertil291utn <bertilt4@gmail.com>
2020-04-24 12:39:38 -0500

    Initialize parametros class - With four local variables

--- lib/parametros.rb
+++ lib/parametros.rb
@@ -1,4 +1,10 @@
 class Parametros
-  def initialize
+  attr_reader :tamano_codigo, :tamano_linea
+
+  def initialize(archivo)
+    @doc = archivo
+    @identacion = 2
+    @tamano_codigo = 140
+    @tamano_linea = 100
   end
 end
