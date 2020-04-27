daf97499f1264263e755ffdee993ebe7cad67a1b
bertil291utn <bertilt4@gmail.com>
2020-04-26 19:15:25 -0500

    Delete action_extra_space_at_end - CSS methods don't care about extra lines after declarations

--- bin/action_listeners.rb
+++ bin/action_listeners.rb
@@ -16,15 +16,6 @@ class ActionListener
     return "More than #{Features::TAMANO_LINEAS_ARCHIVO} lines is not allowed" unless @linter_logic.valid_file_lines?
   end
 
-  def action_extra_space_at_end
-    extra_space = @linter_logic.extra_space_at_end
-    unless extra_space.empty?
-      result = "BLANK SPACES AT THE END OF LINE\n"
-      extra_space.each { |elem| result += 'Line number: ' + elem.to_s + "\n" }
-    end
-    result
-  end
-
   def action_two_empty_lines
     two_space = @linter_logic.two_empty_lines
     unless two_space.empty?
--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,6 @@ require_relative 'action_listeners.rb'
 al = ActionListener.new('./examples/style.css')
 puts al.action_file_name
 puts al.action_valid_file_lines
-puts al.action_extra_space_at_end
 puts al.action_two_empty_lines
 puts al.action_single_row_max_characters
 puts al.action_break_line_after_method
