4f78244d23ec68c02caeda66fc69e5175c95c40b
bertil291utn <bertilt4@gmail.com>
2020-04-27 20:34:43 -0500

    Get file path from console - By deafult is the style.css - Add another file path

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,8 @@
 # !/usr/bin/env ruby
 
 require_relative '../lib/action_listeners.rb'
-al = ActionListener.new('./examples/style.css')
+puts 'Enter your CSS file path: '
+file_path = gets.chomp
+file_path = './examples/style.css' if file_path.empty?
+al = ActionListener.new(file_path)
 puts al.result
