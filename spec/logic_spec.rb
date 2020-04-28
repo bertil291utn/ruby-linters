76b2c55b9b247c0e82d161a30c31e0fefaf96e62
bertil291utn <bertilt4@gmail.com>
2020-04-27 20:14:53 -0500

    Update logic spec file - New divide and conquer methods added in logic file

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,59 +2,116 @@ require_relative '../lib/linter_logic.rb'
 
 RSpec.describe LinterLogic do
   archivo = './examples/style.css'
-  let(:linter_logic) { LinterLogic.new(archivo) }
+  let(:archivo) { Archivo.new(archivo) }
+  let(:linter_logic) { LinterLogic.new }
 
   describe '#two_empty_lines?' do
     it 'returns an array with elements (number of the line where the are more than two empty lines) ' do
-      expect(linter_logic.two_empty_lines).to eq([])
+      resultado = []
+      previous_line = ''
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.two_empty_lines(line, line_num, previous_line)
+        resultado.push(result) unless result.nil?
+        previous_line = line
+      end
+      expect(resultado).to eql([93])
     end
   end
 
   describe '#single_row_max_characters' do
     it 'returns array with elements which has more than total number lines ' do
-      expect(linter_logic.single_row_max_characters).to eq([56])
+      resultado = []
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.single_row_max_characters(line, line_num)
+        resultado.push(result) unless result.nil?
+      end
+      expect(resultado).to eql([56])
     end
   end
 
   describe '#break_line_after_method' do
     it 'returns array with elements which has no empty line after a method ' do
-      expect(linter_logic.break_line_after_method).to eq([])
+      resultado = []
+      previous_line = ''
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.break_line_after_method(line, line_num, previous_line)
+        resultado.push(result) unless result.nil?
+        previous_line = line
+      end
+      expect(resultado).to eql([78])
     end
   end
 
   describe '#break_line_after_comment' do
     it 'returns array with elements which has no empty line after comments ' do
-      expect(linter_logic.break_line_after_comment).to eq([])
+      resultado = []
+      previous_line = ''
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.break_line_after_comment(line, line_num, previous_line)
+        resultado.push(result) unless result.nil?
+        previous_line = line
+      end
+      expect(resultado).to eql([79])
     end
   end
 
   describe '#comments_space' do
     it 'returns array with elements which has no blank spaces after and before comment tags ' do
-      expect(linter_logic.comments_space).to eq([])
+      resultado = []
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.comments_space(line, line_num)
+        resultado.push(result) unless result.nil?
+      end
+      expect(resultado).to eql([78])
     end
   end
 
   describe '#blank_space_after_name' do
     it 'returns array with elements which has no blank spaces before close method brackets  ' do
-      expect(linter_logic.blank_space_after_name).to eq([])
+      resultado = []
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.blank_space_after_name(line, line_num)
+        resultado.push(result) unless result.nil?
+      end
+      expect(resultado).to eql([79])
     end
   end
 
   describe '#repeated_method_name' do
     it 'returns matrix with elements (firstline,secondline, class) which has repeatd classes' do
-      expect(linter_logic.repeated_method_name).to eql([[19, 39, '.titles-font-family']])
+      resultado = []
+      library_add = {}
+      File.foreach(archivo).with_index do |line, line_num|
+        if linter_logic.open_method_name?(line)
+          new_line = linter_logic.clean_mehtod_name(line)
+          result = linter_logic.repeated_method_name(line_num, library_add, new_line)
+          resultado.push(result) unless result.nil?
+        end
+        library_add[line_num + 1] = new_line
+      end
+      expect(resultado).to eql([[19, 39, '.titles-font-family '], [89, 94, '.hidden-section-title ']])
     end
   end
 
   describe '#colon_line' do
     it 'returns array with number lines where there\'s no colon :' do
-      expect(linter_logic.colon_line).to eql([4, 11, 20, 66, 93])
+      resultado = []
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.colon_line(line, line_num)
+        resultado.push(result) unless result.nil?
+      end
+      expect(resultado).to eql([11, 20, 66, 101])
     end
   end
 
   describe '#semicolon_line' do
     it 'returns array with number lines where there\'s no semicolon :' do
-      expect(linter_logic.semicolon_line).to eql([5, 24, 66, 93])
+      resultado = []
+      File.foreach(archivo).with_index do |line, line_num|
+        result = linter_logic.semicolon_line(line, line_num)
+        resultado.push(result) unless result.nil?
+      end
+      expect(resultado).to eql([24, 66, 101])
     end
   end
 end
