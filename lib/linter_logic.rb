e526c902c9a3682c7dbaae9774121e988da83501
bertil291utn <bertilt4@gmail.com>
2020-04-28 10:05:37 -0500

    Remove methods explicit return - Write a variable when it's conditional statements

--- lib/linter_logic.rb
+++ lib/linter_logic.rb
@@ -5,34 +5,37 @@ class LinterLogic
   def initialize; end
 
   def two_empty_lines(line, line_num, previous_line)
-    return (line_num + 1) if empty_line?(line) && empty_line?(previous_line)
+    (line_num + 1) if empty_line?(line) && empty_line?(previous_line)
   end
 
   def single_row_max_characters(line, line_num)
-    return (line_num + 1) if line.to_s.size > Features::TAMANO_LINEA
+    (line_num + 1) if line.to_s.size > Features::TAMANO_LINEA
   end
 
   def break_line_after_method(line, line_num, previous_line)
     close_method = Features::CLOSE_METHOD
     unless line.include?(close_method) && previous_line.include?(close_method)
-      return (line_num + 1) if !empty_line?(line) && previous_line.include?(close_method)
+      result = line_num + 1 if !empty_line?(line) && previous_line.include?(close_method)
     end
+    result
   end
 
   def break_line_after_comment(line, line_num, previous_line)
-    return(line_num + 1) if !empty_line?(line) && comment?(previous_line)
+    (line_num + 1) if !empty_line?(line) && comment?(previous_line)
   end
 
   def comments_space(line, line_num)
     if comment?(line)
-      return (line_num + 1) if !line[2].eql?(' ') || !line[line.length - 4].eql?(' ')
+      result = line_num + 1 if !line[2].eql?(' ') || !line[line.length - 4].eql?(' ')
     end
+    result
   end
 
   def blank_space_after_name(line, line_num)
     if open_method_line?(line)
-      return (line_num + 1) unless line[line.length - 3].eql?(' ')
+      result = line_num + 1 unless line[line.length - 3].eql?(' ')
     end
+    result
   end
 
   def repeated_method_name(line_num, library_add, new_line)
@@ -46,14 +49,16 @@ class LinterLogic
 
   def colon_line(line, line_num)
     if method_rule_line?(line)
-      return (line_num + 1) unless colon?(line)
+      result = line_num + 1 unless colon?(line)
     end
+    result
   end
 
   def semicolon_line(line, line_num)
     if method_rule_line?(line)
-      return(line_num + 1) unless semicolon?(line)
+      result = line_num + 1 unless semicolon?(line)
     end
+    result
   end
 
   def clean_mehtod_name(line)
@@ -65,11 +70,11 @@ class LinterLogic
   def open_method_name?(line)
     open_method_line = Features::OPEN_METHOD
     no_name_initials = Features::NO_NAME_INITIALS
+    result = false
     if !line[0].eql?(' ') && last_char_line(line).eql?(open_method_line)
-      return true if no_name_initials.all? { |elem| !elem.eql?(line[0]) }
+      result = true if no_name_initials.all? { |elem| !elem.eql?(line[0]) }
     end
-
-    false
+    result
   end
 
   private
@@ -77,43 +82,43 @@ class LinterLogic
   def method_rule_line?(line)
     blank_space_count = line.size - line.lstrip.length
     blank_space = blank_space_count.eql?(4) || blank_space_count.eql?(2)
+    result = false
     method_rule_condition = blank_space && !line.include?('{') && !line.include?('}')
-    return true if method_rule_condition
-
-    false
+    result = true if method_rule_condition
+    result
   end
 
   def semicolon?(line)
-    return true if last_char_line(line).eql?(';')
-
-    false
+    result = false
+    result = true if last_char_line(line).eql?(';')
+    result
   end
 
   def colon?(line)
-    return true if line.include?(':')
-
-    false
+    result = false
+    result = true if line.include?(':')
+    result
   end
 
   def open_method_line?(line)
+    result = false
     open_method_line = Features::OPEN_METHOD
-    return true if last_char_line(line).eql?(open_method_line)
-
-    false
+    result = true if last_char_line(line).eql?(open_method_line)
+    result
   end
 
   def empty_line?(line)
+    result = false
     remove_blank = line.length >= 1 ? line.delete(' ') : line
-    return true if remove_blank.delete("\n").length.zero?
-
-    false
+    result = true if remove_blank.delete("\n").length.zero?
+    result
   end
 
   def comment?(line)
     comment_line = Features::COMMENTS
-    return true if line[0].eql?(comment_line) && last_char_line(line).eql?(comment_line)
-
-    false
+    result = false
+    result = true if line[0].eql?(comment_line) && last_char_line(line).eql?(comment_line)
+    result
   end
 
   def last_char_line(line)
