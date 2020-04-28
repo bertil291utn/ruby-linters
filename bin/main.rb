f3ba8249eedf85facebcec26e6e7728a68ee169d
bertil291utn <bertilt4@gmail.com>
2020-04-27 20:15:59 -0500

    Delete debbuging  code

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,5 @@
 # !/usr/bin/env ruby
 
 require_relative '../lib/action_listeners.rb'
-# al = ActionListener.new('./examples/other_sample.css')
 al = ActionListener.new('./examples/style.css')
 puts al.result
